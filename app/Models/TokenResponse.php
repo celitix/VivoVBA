a9a80fbd5160fabd6139d9bce31f0f73872d358a
Arihant Jain <arihantj916@gmail.com>
2025-11-24 15:11:04 +0530

    feat: create model and migration file for token_response

---
+++ app/Models/TokenResponse.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TokenResponse extends Model
+{
+    protected $fillable = [
+        'token_id',
+        'consumer_name',
+        'contact_number',
+        'email',
+        'model',
+        'query',
+        'type',
+    ];
+}
---
+++ database/migrations/2025_11_24_092818_create_token_responses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+
+    General Query / Purchase
+    Consumer Name
+    Contact Number (OTP validation required)
+    Email ID (optional)
+    Model
+     */
+    public function up(): void
+    {
+        Schema::create('token_responses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("token_id")->constrained();
+            $table->string("consumer_name");
+            $table->string("contact_number");
+            $table->string("email");
+            $table->string("model")->nullable();
+            $table->string("query")->nullable();
+            $table->string("type")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('token_responses');
+    }
+};
