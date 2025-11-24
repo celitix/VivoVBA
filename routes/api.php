28c3f6fad987cfa81fcb3a3c2f3f2331bf4979c4
Arihant Jain <arihantj916@gmail.com>
2025-11-24 17:36:39 +0530

    feat: add apis inside middleware

--- routes/api.php
+++ routes/api.php
@@ -4,22 +4,21 @@ use App\Http\Controllers\Token;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/createAdmin', [UserController::class, 'store']);
-Route::post('/login', [UserController::class, 'adminLogin']);
-Route::post('/createUser', [UserController::class, 'createUser']);
-Route::get('/getUserToken/{id}', [UserController::class, 'getUserToken']);
-Route::post('/delete/{id}', [UserController::class, 'destroy']);
 
-
-Route::get("/response/{token}", [Token::class, 'getTokenResponse']);
 Route::post("/save", [Token::class, 'store']);
+Route::post("/sendOtp", [UserController::class, 'sendOtp']);
+Route::post("/verifyOtp", [UserController::class, 'verifyOtp']);
 
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     Route::post('/createAdmin', [UserController::class, 'store']);
-//     Route::post('/createUser', [UserController::class, 'createUser']);
-//     Route::get('/getUserToken/{id}', [UserController::class, 'getUserToken']);
-//     Route::post('/delete/{id}', [UserController::class, 'destroy']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/allUsers', [UserController::class, 'allUsers']);
+    Route::post('/createAdmin', [UserController::class, 'store']);
+    Route::post('/login', [UserController::class, 'adminLogin']);
+    Route::post('/createUser', [UserController::class, 'createUser']);
+    Route::get('/getUserToken/{id}', [UserController::class, 'getUserToken']);
+    Route::post('/delete/{id}', [UserController::class, 'destroy']);
+    // Route::get('/allUsers', [UserController::class, 'allUsers']);
 
-//     Route::get("/response/{token}", [UserController::class, 'getTokenResponse']);
-// });
+
+    Route::get("/response/{token}", [Token::class, 'getTokenResponse']);
+});
