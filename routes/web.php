60a7ae9d91156b4cf16bc3390ea1fe6b7f7427b2
Arihant Jain <arihantj916@gmail.com>
2025-12-06 12:08:10 +0530

    feat: remove  export from web

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/export/{token}', [App\Http\Controllers\TokenController::class, 'export']);
